11911184ffa1f22d25a0f4ebbca903b786e13ad3
Artem Zhavoronkov <azhavoronkov@aeroidea.ru>
2023-07-06 21:55:33 +0400

    update interface

--- cache_interface.go
+++ cache_interface.go
@@ -1,11 +1,17 @@
 package service
 
-// Интерфейс для работы с redis cache
+// RedisInterface Интерфейс для работы с redis cache
 type RedisInterface interface {
-	// Set Cache Value for Tag
-	SetByTag(tag string, value interface{}, expire int)
-	// Get Cache Value for Tag
-	GetByTag(tag string, v interface{}) (result bool)
-	// Delete Cache Value for Tag
-	DeleteByTag(tag string)
+	// SetByTag Set Cache Value for Tag
+	SetByTag(tag string, value interface{}, expire int) error
+	// GetByTag Get Cache Value for Tag
+	GetByTag(tag string, v interface{}) (bool, error)
+	// DeleteByTag Delete Cache Value for Tag
+	DeleteByTag(tag string) error
+	// DeleteTagsByPattern Delete Cache Value for Tag
+	DeleteTagsByPattern(pattern string) error
+	// ClearCache Очистить весь кеш
+	ClearCache() error
+	// ClearCacheByTags Очистить кеш по тегу
+	ClearCacheByTags(tags []string) error
 }
